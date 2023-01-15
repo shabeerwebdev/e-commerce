415dd4c10f96c9b66ddb79dac875096debef602b
shabeerhelloworld <detterminedd@gmail.com>
2023-01-15 20:06:21 +0530

    UI updates

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // export const BASE_URL = "https://crowded-loincloth-crow.cyclic.app/api/";
-export const BASE_URL = "http://localhost:8800/api";
+export const BASE_URL = "https://attractive-plum-nightshirt.cyclic.app";
 
 const register = (username, email, password) => {
   return axios.post(BASE_URL + "/auth/register", {
